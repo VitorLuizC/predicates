290f954af81433e7491b6e05f8ed2957b6193475
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-12-11 10:36:54 -0200

    :sparkles: create isInstanceOf + improve docs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 /**
  * Returns a predicate to check if value is of type (argument).
- * @param type - Argument used to compare type of value.
+ * @example ```js
+ * const isString = isTypeOf('string');
+ * isString(112);
+ * // => false
+ * isString('Hello!');
+ * // => true
+ * ```
+ * @param type
  */
 export const isTypeOf: {
   (type: 'number'): (value: any) => value is number;
@@ -11,3 +18,18 @@ export const isTypeOf: {
   (type: 'function'): (value: any) => value is Function;
   (type: 'undefined'): (value: any) => value is undefined;
 } = (type: string) => (value: any): value is any => typeof value === type;
+
+/**
+ * Returns a predicate to check if value is an instance of constructor (argument).
+ * @example ```js
+ * const isDate = isInstanceOf(Date);
+ * isDate('2018-03-28');
+ * // => false
+ * isDate(new Date('2018-03-28'));
+ * // => true
+ * ```
+ * @param constructor
+ */
+export const isInstanceOf = <T extends Function>(constructor: T) => (value: any): value is T => (
+  value instanceof constructor
+);
