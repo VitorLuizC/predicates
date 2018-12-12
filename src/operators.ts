e86354f2f41651a5af3a29634f280e6e6a292205
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-12-12 12:22:03 -0200

    :sparkles: improve type definitions on operators

--- src/operators.ts
+++ src/operators.ts
@@ -1,43 +1,39 @@
-type Λ<T extends boolean> = (value: any) => T;
+type P<T extends boolean> = (value: any) => T;
 
-type AND = {
+export const AND: {
   <A extends boolean>
-  (λA: Λ<A>): Λ<A>;
+  (pA: P<A>): P<A>;
 
   <A extends boolean, B extends boolean>
-  (λA: Λ<A>, λB: Λ<B>): Λ<A & B>;
+  (pA: P<A>, pB: P<B>): P<A & B>;
 
   <A extends boolean, B extends boolean, C extends boolean>
-  (λA: Λ<A>, λB: Λ<B>, λC: Λ<C>): Λ<A & B & C>;
+  (pA: P<A>, pB: P<B>, pC: P<C>): P<A & B & C>;
 
   <A extends boolean, B extends boolean, C extends boolean, D extends boolean>
-  (λA: Λ<A>, λB: Λ<B>, λC: Λ<C>, λD: Λ<D>): Λ<A & B & C & D>;
+  (pA: P<A>, pB: P<B>, pC: P<C>, pD: P<D>): P<A & B & C & D>;
 
   <A extends boolean, B extends boolean, C extends boolean, D extends boolean, E extends boolean>
-  (λA: Λ<A>, λB: Λ<B>, λC: Λ<C>, λD: Λ<D>, λE: Λ<E>): Λ<A & B & C & D & E>;
+  (pA: P<A>, pB: P<B>, pC: P<C>, pD: P<D>, pE: P<E>): P<A & B & C & D & E>;
 
-  (...λs: Λ<boolean>[]): Λ<boolean>;
-};
+  (...ps: P<boolean>[]): P<boolean>;
+} = (...ps: P<boolean>[]) => (value: any) => ps.every((p) => p(value));
 
-export const AND: AND = (...λs: Λ<boolean>[]) => (value: any) => λs.every((λ) => λ(value));
-
-type OR = {
+export const OR: {
   <A extends boolean>
-  (λA: Λ<A>): Λ<A>;
+  (pA: P<A>): P<A>;
 
   <A extends boolean, B extends boolean>
-  (λA: Λ<A>, λB: Λ<B>): Λ<A | B>;
+  (pA: P<A>, pB: P<B>): P<A | B>;
 
   <A extends boolean, B extends boolean, C extends boolean>
-  (λA: Λ<A>, λB: Λ<B>, λC: Λ<C>): Λ<A | B | C>;
+  (pA: P<A>, pB: P<B>, pC: P<C>): P<A | B | C>;
 
   <A extends boolean, B extends boolean, C extends boolean, D extends boolean>
-  (λA: Λ<A>, λB: Λ<B>, λC: Λ<C>, λD: Λ<D>): Λ<A | B | C | D>;
+  (pA: P<A>, pB: P<B>, pC: P<C>, pD: P<D>): P<A | B | C | D>;
 
   <A extends boolean, B extends boolean, C extends boolean, D extends boolean, E extends boolean>
-  (λA: Λ<A>, λB: Λ<B>, λC: Λ<C>, λD: Λ<D>, λE: Λ<E>): Λ<A | B | C | D | E>;
+  (pA: P<A>, pB: P<B>, pC: P<C>, pD: P<D>, pE: P<E>): P<A | B | C | D | E>;
 
-  (...λs: Λ<boolean>[]): Λ<boolean>;
-};
-
-export const OR: OR = (...λs: Λ<boolean>[]) => (value: any) => λs.some((λ) => λ(value));
+  (...ps: P<boolean>[]): P<boolean>;
+} = (...ps: P<boolean>[]) => (value: any) => ps.some((p) => p(value));
