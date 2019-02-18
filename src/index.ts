a9bfe073976941cdc79dd2bb7ceb7af49e412f2a
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2019-02-18 16:15:56 -0300

    :sparkles: create and export Predicate function type

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export * from './predicates';
+export { default as Predicate } from './model/Predicate';
---
+++ src/model/Predicate.ts
@@ -0,0 +1,9 @@
+/**
+ * A function that returns `true` if value matches a check and `false`
+ * otherwise (AKA Check Function).
+ * @example
+ * const isNull: Predicate = (value: any) => value === null;
+ */
+type Predicate = (value: any) => boolean;
+
+export default Predicate;
